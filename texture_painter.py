f0422e496c2f3a1f07a2549aec449e4712c77433
tdvance <tdvance@gmail.com>
2016-11-04 10:12:12 -0400

    Initial Commit

---
+++ texture_painter.py
@@ -0,0 +1,14 @@
+"""
+Renders text from a CSV file to textures and applies them to multiple
+objects.
+
+Use snippets...
+
+#at start of blender:
+
+>>> import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
+
+#on change:
+>>> import importlib; importlib.reload(texture_painter)
+"""
+
